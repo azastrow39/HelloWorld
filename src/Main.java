cadd65d5db50918e0b4cdd3d2ec18dcf49b34e67
Walter Mays <sircular17@gmail.com>
2017-09-07 22:54:55 -0500

    Added my name to the program.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("Hello World!\nThis program was made by Adam Zastrow");
+        System.out.printf("Hello World!\nThis program was made by Adam Zastrow and Walter Mays");
     }
 }
